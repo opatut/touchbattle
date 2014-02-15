1f5bf88ab70e71e8a8f3cc1fa2258890eb58d571
Paul Bienkowski <opatutlol@aol.com>
2014-02-15 12:40:38 +0100

    Awesome particles

--- main.lua
+++ main.lua
@@ -3,6 +3,11 @@ require("util/resources")
 require("scene/world")
 require("scene/entity")
 
+COLORS = {
+    {255, 200, 0},
+    {0, 100, 255}
+}
+
 Particle = class("Particle", Entity)
 function Particle:__init(pos, vel, color)
     Entity.__init(self)
@@ -33,12 +38,25 @@ Player = class("Player")
 
 function Player:__init(id)
     self.world = World()
-    self.color = {255 * (1 - id), 255 * id, 0}
-    self.offset = Vector(love.graphics.getWidth() * id, love.graphics.getHeight() / 2)
+    self.color = COLORS[id+1]
     self.rotation = math.pi / 2 - math.pi * id
     self.id = id
     self.activity = 0
     self.cooldown = 0
+    self.lastActive = -1000
+
+    self.p = love.graphics.newParticleSystem(resources.images.blur)
+    self.p:setColors(
+        self.color[1], self.color[2], self.color[3], 255, 
+        --self.color[1], self.color[2], self.color[3], 100, 
+        255, 255, 255, 0)
+    self.p:setParticleLifetime(0.8, 1)
+    self.p:setDirection(-math.pi / 2)
+    self.p:setSpread(-0.5, 0.5)
+    self.p:setSpeed(300, 400)
+    self.p:setRadialAcceleration(-300)
+    self.p:setSizes(1, 0.2, 2)
+    self.p:setAreaSpread("normal", 100, 0)
 end
 
 function Player:draw()
@@ -46,39 +64,62 @@ function Player:draw()
     love.graphics.translate(self.offset.x, self.offset.y)
     love.graphics.scale(SCALE, SCALE)
     love.graphics.rotate(self.rotation)
-    love.graphics.setScissor(self.id * love.graphics.getWidth() / 2, 0, love.graphics.getWidth() / 2, love.graphics.getHeight())
+    --love.graphics.setScissor(self.id * love.graphics.getWidth() / 2, 0, love.graphics.getWidth() / 2, love.graphics.getHeight())
+
+
+    -- background
+    if TIME - self.lastActive < 0.02 then
+        love.graphics.setColor(255, 255, 255, 20)
+        love.graphics.rectangle("fill", -100, -HEIGHT, 200, HEIGHT)
+    end
 
     -- entities
     self.world:draw()
 
     -- text
-    local t = state .. " " .. math.round(self.activity, 3)
+    local t = state
     love.graphics.setFont(resources.fonts.default)
     love.graphics.setColor(255, 255, 255)
     local s = 1/(SCALE * (1 + 0.1 * getPulse()))
     love.graphics.print(t, -resources.fonts.default:getWidth(t)/2*s, -HEIGHT+10, 0, s, s)
-    love.graphics.rectangle("fill", -0.1, -HEIGHT+0.01, 0.2, 0.5)
 
-    -- circle
+    -- gradient
     love.graphics.setColor(unpack(self.color))
-    love.graphics.circle("fill", 0, 0, 10, 32)
+    local h = self.activity * HEIGHT * 0.1
+    love.graphics.draw(resources.images.gradient, -100, -h, 0, 100, h/2)
+
+    love.graphics.setBlendMode("additive")
+    love.graphics.draw(self.p)
+    love.graphics.setBlendMode("alpha")
 
     love.graphics.pop()
 end
 
 function Player:update(dt)
+    -- fix offset when rotating
+    self.offset = Vector(love.graphics.getWidth() * self.id, love.graphics.getHeight() / 2)
+
     self.world:update(dt)
-    self.activity = math.max(0, self.activity * (1 - dt))
+    self.activity = math.max(0, self.activity * (1 - dt*1))
 
     self.cooldown = self.cooldown - dt
     if self.cooldown < 0 then
-        self.world:add(Particle(Vector(0, 0), Vector(0, -300), self.color))
+        -- self.world:add(Particle(Vector(0, 0), Vector(0, -300), self.color))
         self.cooldown = 1/self.activity
     end
+
+    if self.activity < 0.01 then
+        self.p:pause()
+    else
+        self.p:start()
+        self.p:setEmissionRate(500 * self.activity)
+    end
+    self.p:update(dt)
 end
 
-function Player:active()
-    self.activity = math.min(1, self.activity + 0.1)
+function Player:active(strength, x)
+    self.activity = math.min(1, self.activity + strength)
+    self.lastActive = TIME
 end
 
 --------------------------------------------------------------------------------
@@ -86,8 +127,7 @@ end
 resources = Resources("data/")
 state = "TAP"
 TIME = 0
-players = {Player(0), Player(1)}
-
+players = {}
 
 --------------------------------------------------------------------------------
 
@@ -102,7 +142,11 @@ end
 function love.load()
     resources:addFont("default", "BD_Cartoon_Shout.ttf", 80)
     resources:addImage("blur", "blur.png")
+    resources:addImage("star", "star.png")
+    resources:makeGradientImage("gradient", {255, 255, 255, 0}, {255, 255, 255, 255}, false)
     resources:load()
+
+    players = {Player(0), Player(1)}
 end
 
 function love.update(dt)
@@ -117,11 +161,18 @@ end
 
 function love.touchpressed(id, x, y, p)
     local p = x < 0.5 and 1 or 2
-    players[p]:active()
+    local yp = 200 * y - 100
+    players[p]:active(0.1, p == 1 and yp or -yp)
 end
 
 function love.draw()
     for i,p in pairs(players) do
         p:draw()
     end 
+
+    love.graphics.setColor(255, 255, 255, 100)
+    local n = 20.5
+    for i=0,n do
+        love.graphics.line(love.graphics.getWidth() / 2, love.graphics.getHeight() * i / n, love.graphics.getWidth() / 2, love.graphics.getHeight() * (i+0.5) / n)
+    end
 end
